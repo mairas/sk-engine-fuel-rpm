a185ac2a3838537d5f5b0f26d7ab249226227796
Matti Airas <mairas@iki.fi>
2020-09-28 16:06:34 +0300

    Support both ESP8266 and ESP32

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,17 @@
 #include "transforms/frequency.h"
 #include "wiring_helpers.h"
 
+
+#if defined(ESP8266)
+  #define IN_FLOW_PIN D1
+  #define RETURN_FLOW_PIN D2
+  #define RPM_PIN D5
+#elif defined(ESP32)
+  #define IN_FLOW_PIN 16
+  #define RETURN_FLOW_PIN 17
+  #define RPM_PIN 18
+#endif
+
 ReactESP app([] () {
   #ifndef SERIAL_DEBUG_DISABLED
   Serial.begin(115200);
@@ -20,7 +31,6 @@ ReactESP app([] () {
 
   auto builder = SensESPAppBuilder();
 
-  setup_fuel_flow_meter(D1, D2);
   sensesp_app = builder
     .set_wifi("Sample SSID", "wifi_passphrase")
     ->set_sk_server("sk-server.local", 80)
@@ -28,7 +38,9 @@ ReactESP app([] () {
     ->set_standard_sensors(ALL)
     ->get_app();
 
-  (new DigitalInputCounter(D5, INPUT_PULLUP, RISING, 500))
+  setup_fuel_flow_meter(IN_FLOW_PIN, RETURN_FLOW_PIN);
+
+  (new DigitalInputCounter(RPM_PIN, INPUT_PULLUP, RISING, 500))
       ->connectTo(new Frequency(1. / 97., "/sensors/engine_rpm/calibration"))
       ->connectTo(new SKOutput<float>("propulsion.main.revolutions", "/sensors/engine_rpm/sk"));
 
